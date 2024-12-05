ed9a07481fd7130b4b615762c074ea3e56354d13
Robert Blenkinsopp <robert@blenkinsopp.net>
2024-12-05 21:00:49 +0000

    Attempt to make day 3 quicker (again)

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -5,40 +5,25 @@ fn main() {
     let data = get_input_as_string();
 
     // Compile a suitable regex to match uncorrupted multiplications.
-    let regex = Regex::new(r"(?m)(do|don't|mul)\((?:(\d{1,3}),(\d{1,3}))?\)").unwrap();
+    let regex = Regex::new(r"(?m)(do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\))").unwrap();
 
     // Iterate over the mul statements and accumulate the result, tracking mul enablement.
     let mut part1_result = 0;
     let mut part2_result = 0;
     let mut bool_mul_enabled = true;
 
-    // Nice version
-    //for instruction in regex.captures_iter(&data) {
-    //    match instruction.get(1).unwrap().as_str() {
-    //        "do" => bool_mul_enabled = true,
-    //        "don't" => bool_mul_enabled = false,
-    //        "mul" => {
-    //            let (a, b) = (instruction[2].parse::<i32>().unwrap(), instruction[3].parse::<i32>().unwrap());
-    //            let mul_result = a * b;
-    //            part1_result += mul_result;
-    //            if bool_mul_enabled {
-    //                part2_result += mul_result;
-    //            }
-    //        },
-    //        &_ => panic!()
-    //    }
-    //}
-
     // Hacky version for speed.
     for instruction in regex.captures_iter(&data) {
-        if instruction.get(2).is_none() {
-            bool_mul_enabled = instruction.get(1).unwrap().len() == 2;
-        } else {
-            let (a, b) = (instruction[2].parse::<i32>().unwrap(), instruction[3].parse::<i32>().unwrap());
-            let mul_result = a * b;
-            part1_result += mul_result;
-            if bool_mul_enabled {
-                part2_result += mul_result;
+        match instruction.get(0).unwrap().len() {
+            4 => bool_mul_enabled = true,  // Do() 
+            7 => bool_mul_enabled = false, // Don't()
+            _ => {
+                let (a, b) = (instruction[2].parse::<i32>().unwrap(), instruction[3].parse::<i32>().unwrap());
+                let mul_result = a * b;
+                part1_result += mul_result;
+                if bool_mul_enabled {
+                    part2_result += mul_result;
+                }
             }
         }
     }
