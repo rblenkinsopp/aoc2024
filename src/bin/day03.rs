594bed7abaab81cec395f97e377483fd3255eb23
Robert Blenkinsopp <robert@blenkinsopp.net>
2024-12-05 20:50:23 +0000

    Attempt to make day 3 quicker

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -11,21 +11,38 @@ fn main() {
     let mut part1_result = 0;
     let mut part2_result = 0;
     let mut bool_mul_enabled = true;
+
+    // Nice version
+    //for instruction in regex.captures_iter(&data) {
+    //    match instruction.get(1).unwrap().as_str() {
+    //        "do" => bool_mul_enabled = true,
+    //        "don't" => bool_mul_enabled = false,
+    //        "mul" => {
+    //            let (a, b) = (instruction[2].parse::<i32>().unwrap(), instruction[3].parse::<i32>().unwrap());
+    //            let mul_result = a * b;
+    //            part1_result += mul_result;
+    //            if bool_mul_enabled {
+    //                part2_result += mul_result;
+    //            }
+    //        },
+    //        &_ => panic!()
+    //    }
+    //}
+
+    // Hacky version for speed.
     for instruction in regex.captures_iter(&data) {
-        match instruction.get(1).unwrap().as_str() {
-            "do" => bool_mul_enabled = true,
-            "don't" => bool_mul_enabled = false,
-            "mul" => {
-                let (a, b) = (instruction[2].parse::<i32>().unwrap(), instruction[3].parse::<i32>().unwrap());
-                let mul_result = a * b;
-                part1_result += mul_result;
-                if bool_mul_enabled {
-                    part2_result += mul_result;
-                }
-            },
-            &_ => panic!()
+        if instruction.get(2).is_none() {
+            bool_mul_enabled = instruction.get(1).unwrap().len() == 2;
+        } else {
+            let (a, b) = (instruction[2].parse::<i32>().unwrap(), instruction[3].parse::<i32>().unwrap());
+            let mul_result = a * b;
+            part1_result += mul_result;
+            if bool_mul_enabled {
+                part2_result += mul_result;
+            }
         }
     }
+
     println!("{}", part1_result);
     println!("{}", part2_result);
 }
