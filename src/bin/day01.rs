32c1609997ab8630ad73d1a7803feab7c42092c5
Robert Blenkinsopp <robert@blenkinsopp.net>
2024-12-05 21:05:32 +0000

    Switch to radix sort on day 1

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -3,42 +3,43 @@ use itertools::Itertools;
 use std::collections::HashMap;
 use std::io::BufRead;
 use aoc2024::get_input_reader;
+use rdxsort::*;
 
 // Pre-allocate 1000 entries as this is the expected input count.
 const EXPECTED_INPUT_COUNT: usize = 1000;
 
 fn main() -> anyhow::Result<()> {
     // Parse the data into sorted lists of integers and count the right numbers.
-    let mut left_numbers: Vec<i32> = Vec::with_capacity(EXPECTED_INPUT_COUNT);
-    let mut right_numbers: Vec<i32> = Vec::with_capacity(EXPECTED_INPUT_COUNT);
-    let mut right_numbers_counts: HashMap<i32, i32> = HashMap::with_capacity(EXPECTED_INPUT_COUNT);
+    let mut left_numbers: Vec<u32> = Vec::with_capacity(EXPECTED_INPUT_COUNT);
+    let mut right_numbers: Vec<u32> = Vec::with_capacity(EXPECTED_INPUT_COUNT);
+    let mut right_numbers_counts: HashMap<u32, u32> = HashMap::with_capacity(EXPECTED_INPUT_COUNT);
     
     
     for line in get_input_reader().lines() {
         let (left, right) = line?
             .split_whitespace()
-            .map(|s| s.parse::<i32>().unwrap())
+            .map(|s| s.parse::<u32>().unwrap())
             .collect_tuple()
             .unwrap();
         left_numbers.push(left);
         right_numbers.push(right);
         *right_numbers_counts.entry(right).or_insert(0) += 1;
     }
-    left_numbers.sort_unstable();
-    right_numbers.sort_unstable();
+    left_numbers.rdxsort();
+    right_numbers.rdxsort();
 
     // Part 1
     // Calculate the accumulation of the difference between the two numbers.
-    let difference_sum: i32 = left_numbers
+    let difference_sum: u32 = left_numbers
         .iter()
         .zip(right_numbers)
-        .map(|(left, right)| (left - right).abs())
+        .map(|(left, right)| left.abs_diff(right))
         .sum();
     println!("{}", difference_sum);
 
     // Part 2
     // Calculate the similarity score.
-    let similarity_score: i32 = left_numbers.iter()
+    let similarity_score: u32 = left_numbers.iter()
         .filter_map(|&number| right_numbers_counts.get(&number).map(|&count| number * count))
         .sum();
     println!("{}", similarity_score);
