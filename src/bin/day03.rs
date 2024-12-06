cb8c266bbdfbcceedd378e3d718e0114017e27f0
Robert Blenkinsopp <robert@blenkinsopp.net>
2024-12-06 09:55:53 +0000

    Optimised the day 3 regex a little

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -4,21 +4,20 @@ use regex::Regex;
 fn main() {
     let data = get_input_as_string();
 
-    // Compile a suitable regex to match uncorrupted multiplications.
-    let regex = Regex::new(r"(?m)(do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\))").unwrap();
-
     // Iterate over the mul statements and accumulate the result, tracking mul enablement.
     let mut part1_result = 0;
     let mut part2_result = 0;
     let mut bool_mul_enabled = true;
-
-    // Hacky version for speed.
+    
+    // Hacky Regex version for speed.
+    // Compile a suitable regex to match uncorrupted multiplications.
+    let regex = Regex::new(r"(?m)(do|don't|mul)\((?:(\d{1,3}),(\d{1,3}))?\)").unwrap();
     for instruction in regex.captures_iter(&data) {
-        match instruction.get(0).unwrap().len() {
-            4 => bool_mul_enabled = true,  // Do() 
-            7 => bool_mul_enabled = false, // Don't()
+        match instruction.get(1).unwrap().len() {
+            2 => bool_mul_enabled = true,  // do
+            5 => bool_mul_enabled = false, // don't
             _ => {
-                let (a, b) = (instruction[2].parse::<i32>().unwrap(), instruction[3].parse::<i32>().unwrap());
+                let (a, b) = (instruction[2].parse::<u32>().unwrap(), instruction[3].parse::<u32>().unwrap());
                 let mul_result = a * b;
                 part1_result += mul_result;
                 if bool_mul_enabled {
