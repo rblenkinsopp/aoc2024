ef5707876d3e76a1e9f65aac82c0a1a746810580
Robert Blenkinsopp <robert@blenkinsopp.net>
2024-12-13 14:12:45 +0000

    Attempt stack based day 1

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,18 +1,20 @@
 // Advent of Code 2024: Day 1
 use itertools::Itertools;
-use std::collections::HashMap;
 use std::io::BufRead;
 use aoc2024::{get_input_reader, print_answer};
 use arrayvec::ArrayVec;
+use rdst;
+use rdst::RadixSort;
 
 // Pre-allocate 1000 entries as this is the expected input count.
 const EXPECTED_INPUT_COUNT: usize = 1000;
+const EXPECTED_MAX_VALUE:usize = 99999;
 
 fn main() {
     // Parse the data into sorted lists of integers and count the right numbers.
-    let mut left_numbers: ArrayVec<u32, EXPECTED_INPUT_COUNT> = ArrayVec::new_const();
-    let mut right_numbers: ArrayVec<u32, EXPECTED_INPUT_COUNT> = ArrayVec::new_const();
-    let mut right_numbers_counts: HashMap<u32, u32> = HashMap::with_capacity(EXPECTED_INPUT_COUNT);
+    let mut left_numbers: ArrayVec<u32, EXPECTED_INPUT_COUNT> = ArrayVec::new();
+    let mut right_numbers: ArrayVec<u32, EXPECTED_INPUT_COUNT> = ArrayVec::new();
+    let mut right_numbers_counts: ArrayVec<u32,EXPECTED_MAX_VALUE> = ArrayVec::new();
 
     // Input will always be valid so ignore additional safety checks
     unsafe {
@@ -24,11 +26,11 @@ fn main() {
                 .unwrap_unchecked();
             left_numbers.push(left);
             right_numbers.push(right);
-            *right_numbers_counts.entry(right).or_insert(0) += 1;
+            right_numbers_counts[right as usize] += 1;
         }
     }
-    left_numbers.sort_unstable();
-    right_numbers.sort_unstable();
+    left_numbers.radix_sort_unstable();
+    right_numbers.radix_sort_unstable();
 
     // Part 1
     // Calculate the accumulation of the difference between the two numbers.
@@ -42,7 +44,7 @@ fn main() {
     // Part 2
     // Calculate the similarity score.
     let similarity_score: u32 = left_numbers.iter()
-        .filter_map(|&number| right_numbers_counts.get(&number).map(|&count| number * count))
+        .filter_map(|&number| right_numbers_counts.get(number as usize).map(|&count| number * count))
         .sum();
 
     print_answer(difference_sum, similarity_score);
