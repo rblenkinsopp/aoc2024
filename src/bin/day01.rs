f1d4c00a18dbef03db757f8a97dcfdbcefd4c791
Robert Blenkinsopp <robert@blenkinsopp.net>
2024-12-13 16:43:01 +0000

    Remove unsafe code from day 1

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,9 +1,8 @@
 // Advent of Code 2024: Day 1
-use itertools::Itertools;
 use std::io::BufRead;
-use aoc2024::{get_input_reader, print_answer};
+use aoc2024::get_input_reader;
 use arrayvec::ArrayVec;
-use rdst;
+use fxhash::{FxBuildHasher, FxHashMap};
 use rdst::RadixSort;
 
 // Pre-allocate 1000 entries as this is the expected input count.
@@ -14,38 +13,37 @@ fn main() {
     // Parse the data into sorted lists of integers and count the right numbers.
     let mut left_numbers: ArrayVec<u32, EXPECTED_INPUT_COUNT> = ArrayVec::new();
     let mut right_numbers: ArrayVec<u32, EXPECTED_INPUT_COUNT> = ArrayVec::new();
-    let mut right_numbers_counts: Vec<u32> = vec![0; EXPECTED_MAX_VALUE];
+    let mut right_numbers_counts: FxHashMap<u32, u32> = FxHashMap::with_capacity_and_hasher(EXPECTED_MAX_VALUE, FxBuildHasher::default());
 
-    // Input will always be valid so ignore additional safety checks
-    unsafe {
-        for line in get_input_reader().lines() {
-            let (left, right) = line.unwrap_unchecked()
-                .split_whitespace()
-                .map(|s| s.parse::<u32>().unwrap_unchecked())
-                .collect_tuple()
-                .unwrap_unchecked();
-            left_numbers.push(left);
-            right_numbers.push(right);
-            right_numbers_counts[right as usize] += 1;
-        }
+    for line in get_input_reader().lines() {
+        let line = line.unwrap();
+        let mut parts = line.split_whitespace();
+
+        let left = parts.next().unwrap().parse::<u32>().unwrap();
+        left_numbers.push(left);
+
+        let right = parts.next().unwrap().parse::<u32>().unwrap();
+        right_numbers.push(right);
+        *right_numbers_counts.entry(right).or_insert(0) += 1;
     }
     left_numbers.radix_sort_unstable();
     right_numbers.radix_sort_unstable();
 
     // Part 1
     // Calculate the accumulation of the difference between the two numbers.
-    let difference_sum: u32 = left_numbers
-        .iter()
-        .zip(right_numbers)
-        .map(|(left, right)| left.abs_diff(right))
-        .sum();
-
+    let mut difference_sum: u32 = 0;
+    for (left, right) in left_numbers.iter().zip(&right_numbers) {
+        difference_sum += left.abs_diff(*right);
+    }
 
     // Part 2
     // Calculate the similarity score.
-    let similarity_score: u32 = left_numbers.iter()
-        .filter_map(|&number| right_numbers_counts.get(number as usize).map(|&count| number * count))
-        .sum();
+    let mut similarity_score: u32 = 0;
+    for &number in &left_numbers {
+        if let Some(&count) = right_numbers_counts.get(&number) {
+            similarity_score += number * count;
+        }
+    }
 
-    print_answer(difference_sum, similarity_score);
+    println!("{}\n{}", difference_sum, similarity_score);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,71 +1,13 @@
 use std::fs::File;
-use std::io::{BufReader, StdoutLock, Write};
-use std::{env, fs, ptr};
+use std::io::{BufReader};
+use std::{env, fs};
 
 #[inline]
 pub fn get_input_reader() -> BufReader<File> {
-    unsafe { BufReader::new(File::open(env::args().nth(1).unwrap_unchecked()).unwrap_unchecked()) }
+    BufReader::new(File::open(env::args().nth(1).unwrap()).unwrap())
 }
 
 #[inline]
 pub fn get_input_as_string() -> String {
-    unsafe {
-        fs::read_to_string(env::args().nth(1).unwrap_unchecked()).unwrap_unchecked()
-    }
-}
-
-#[inline]
-pub fn print_answer(part1: u32, part2: u32) {
-    let stdout = std::io::stdout();
-    let mut stdout_lock = stdout.lock();
-
-    fast_print_u32(&mut stdout_lock, part1);
-    unsafe { stdout_lock.write_all(b"\n").unwrap_unchecked(); }
-    fast_print_u32(&mut stdout_lock, part2);
-}
-
-#[inline]
-fn fast_print_u32(stdout_lock: &mut StdoutLock, mut n: u32) {
-    const DIGITS_LUT: &[u8; 200] = b"0001020304050607080910111213141516171819\
-                                     2021222324252627282930313233343536373839\
-                                     4041424344454647484950515253545556575859\
-                                     6061626364656667686970717273747576777879\
-                                     8081828384858687888990919293949596979899";
-
-    // Buffer of max 10 bytes for storing digits of a u32
-    let mut buffer = [0u8; 10];
-    let mut i = buffer.len();
-
-    // Process digits two at a time for fast handling of larger numbers
-    while n >= 100 {
-        let rem = (n % 100) as usize;
-        n /= 100;
-        i -= 2;
-        unsafe {
-            ptr::copy_nonoverlapping(
-                DIGITS_LUT.as_ptr().add(rem * 2),
-                buffer.as_mut_ptr().add(i),
-                2,
-            );
-        }
-    }
-
-    // Handle the remaining one or two digits (0-99)
-    if n < 10 {
-        i -= 1;
-        buffer[i] = b'0' + n as u8;
-    } else {
-        let rem = (n % 100) as usize;
-        i -= 2;
-        unsafe {
-            ptr::copy_nonoverlapping(
-                DIGITS_LUT.as_ptr().add(rem * 2),
-                buffer.as_mut_ptr().add(i),
-                2,
-            );
-        }
-    }
-
-    // Write only the filled portion of the buffer
-    unsafe { stdout_lock.write_all(&buffer[i..]).unwrap_unchecked(); }
+    fs::read_to_string(env::args().nth(1).unwrap()).unwrap()
 }
